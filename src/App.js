1b98ae3da164ee34e66a17deaab4debe737e6075
buketkonuk <bukikir@gmail.com>
2019-04-30 10:36:28 +0100

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './buket_pic.jpg';
 import './App.css';
 
 function App() {
@@ -8,16 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Buket Konuk-Hirst Personal Portfolio Page.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <div>Home</div>
+          <div>Projects</div>
+          <div>gitHub</div>
+          <div>Contact</div>
+        </div>
       </header>
     </div>
   );
